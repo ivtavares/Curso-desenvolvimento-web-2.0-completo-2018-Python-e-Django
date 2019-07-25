870162659c877be126310ad0d243aec38fd3e360
Igor <iv_tavares@gmail.com>
2019-07-25 00:17:31 -0300

    Refatorando o tempate do site para Bootstrap

--- ProjetoFinal/website/models.py
+++ ProjetoFinal/website/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Contato(models.Model):
+    nome = models.CharField(max_length=100)
+    sobrenome = models.CharField(max_length=100)
+    endereco = models.CharField(max_length=100)
+    mensagem = models.TextField()
+    receber_noticias = models.BooleanField()
+    email = models.EmailField(max_length=254)
+
+    def __str__(self):
+        return self.nome
